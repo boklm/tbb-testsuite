c882ddc3358ca5634da9f6ec550c2a151b1c04a1
Nicolas Vigier <boklm@mars-attacks.org>
2014-02-21 20:24:12 +0100

    Add a mozmill test to visit check.tpo and take a screenshot

---
+++ mozmill-tests/tbb-tests/tbbScreenshot.js
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Include this if opening relocatable, potentially remotely located files
+const BASE_URL = collector.addHttpResource("../mozilla-mozmill-tests/data/");
+const TEST_DATA = BASE_URL + "path/file.html";
+
+// Include this if opening external urls
+// const TEST_DATA = "http://www.external.url/path/file.html";
+
+// Include this only if specifying a timeout other than 5000ms
+const TEST_EXAMPLE_TIMEOUT = 3000;
+
+var screenshot = require("../mozilla-mozmill-tests/firefox/lib/screenshot");
+
+// Setup for the test
+var setupModule = function(aModule) {
+  aModule.controller = mozmill.getBrowserController();
+}
+
+// Run the test
+var testStartTBB = function() {
+    controller.open('http://check.torproject.org');
+    controller.waitForPageLoad();
+    screenshot.create(controller, []);
+}
