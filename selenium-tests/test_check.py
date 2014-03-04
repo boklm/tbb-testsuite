dab869b0eeb9516b27f241d9029ecca254ebd665
Nicolas Vigier <boklm@mars-attacks.org>
2014-03-04 11:51:55 +0100

    Add a run_test script to run selenium tests

--- selenium-tests/test_check.py
+++ selenium-tests/test_check.py
@@ -2,11 +2,9 @@
 import unittest, time, re
 import tbbtest
 
-class CheckTpo(tbbtest.TBBTest):
+class Test(tbbtest.TBBTest):
     def test_check_tpo(self):
         driver = self.driver
         driver.get("http://check.torproject.org/")
         self.assertEqual("Congratulations. This browser is configured to use Tor.", driver.find_element_by_css_selector("h1.on").text)
 
-if __name__ == "__main__":
-    unittest.main()
