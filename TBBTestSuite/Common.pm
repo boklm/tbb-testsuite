b819e12b427a34b82462d02a90a8a2552b5825e1
Nicolas Vigier <boklm@mars-attacks.org>
2014-03-12 18:48:01 +0100

    Add system infos to report.yml

--- TBBTestSuite/Common.pm
+++ TBBTestSuite/Common.pm
@@ -2,18 +2,38 @@ package TBBTestSuite::Common;
 
 use warnings;
 use strict;
+use IO::CaptureOutput qw(capture_exec);
 
 our (@ISA, @EXPORT_OK);
 BEGIN {
     require Exporter;
     @ISA       = qw(Exporter);
-    @EXPORT_OK = qw(exit_error);
+    @EXPORT_OK = qw(exit_error system_infos);
 }
 
 sub exit_error {
     print STDERR "Error: ", $_[0], "\n";
     chdir '/';
     exit (exists $_[1] ? $_[1] : 1);
+}
+
+sub system_infos {
+    my %res;
+    my $lsbr = '/usr/bin/lsb_release';
+    ($res{arch}) = capture_exec('uname', '-m');
+    chomp $res{arch};
+    if (-f $lsbr) {
+        my ($id) = capture_exec($lsbr, '-i');
+        $id =~ s/^Distributor ID:\s+//;
+        chomp $id;
+        my ($release) = capture_exec($lsbr, '-r');
+        $release =~ s/^Release:\s+//;
+        chomp $release;
+        $res{osname} = $id . $release;
+    } else {
+        $res{osname} = 'Linux';
+    }
+    return \%res;
 }
 
 1;
