8ea482844c634a81c7969dad87582244b9fdc0e3
Nicolas Vigier <boklm@mars-attacks.org>
2014-03-21 16:34:20 +0100

    Add screenshots of preferences windows

--- mozmill-tests/tbb-tests/screenshots.js
+++ mozmill-tests/tbb-tests/screenshots.js
@@ -10,4 +10,14 @@ var setupModule = function(aModule) {
 var testStartTBB = function() {
     common.load_page(controller, 'http://check.torproject.org');
     screenshot.create(controller, []);
+    const URLs = [
+        'chrome://browser/content/preferences/preferences.xul',
+        'chrome://torbutton/content/preferences.xul',
+        'chrome://torlauncher/content/network-settings-wizard.xul',
+        ];
+    for (var i=0; i < URLs.length; i++) {
+        controller.open(URLs[i]);
+        controller.waitForPageLoad();
+        screenshot.create(controller.browserObject.selectedBrowser.contentWindow, []);
+    }
 }
