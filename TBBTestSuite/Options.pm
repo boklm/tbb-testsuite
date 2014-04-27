e27d74342010c5c72cef8c930817c60d96a5f072
Nicolas Vigier <boklm@torproject.org>
2014-04-27 12:48:48 +0200

    Add the reports-url option

--- TBBTestSuite/Options.pm
+++ TBBTestSuite/Options.pm
@@ -47,7 +47,7 @@ sub get_options {
                      virtualenv=s xvfb! name=s download-dir=s config=s
                      action=s enable-tests=s upload-to=s os=s arch=s
                      virustotal! email-to=s@ email-from=s email-subject=s
-                     mozmill-dir=s);
+                     mozmill-dir=s reports-url=s);
     my (%cli, %config);
     Getopt::Long::GetOptionsFromArray(\@_, \%cli, @options) || exit 1;
     $cli{args} = \@_ if @_;
