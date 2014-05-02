d5c3252eff148609f8d663cd5f2b9a6953416c6e
Nicolas Vigier <boklm@torproject.org>
2014-05-02 15:18:27 +0200

    use tor-browser-bundle.git tags to know the latest version to test
    We're now cloning builders/tor-browser-bundle.git and using the latest
    signed tag on the master branch to find the current version we need to
    test.
    
    We are also using the sha256sum of the file sha256sums.txt as a build
    identifier, and running the tests again when that identifier changes
    (when a new build of the same version is available).

---
+++ TBBTestSuite/TBBVersion.pm
@@ -0,0 +1,91 @@
+package TBBTestSuite::TBBVersion;
+
+use warnings;
+use strict;
+use FindBin;
+use TBBTestSuite::Common qw(exit_error);
+use Cwd qw(getcwd);
+use File::Slurp;
+use IO::CaptureOutput qw(capture_exec);
+use LWP::Simple;
+use Digest::SHA qw(sha256_hex);
+
+my $options;
+my $tbbgit_url = 'https://git.torproject.org/builders/tor-browser-bundle.git';
+my @sign_users = ( 'mikeperry', 'gk', );
+my @tbb_builders = ( 'mikeperry', 'gk', 'linus', 'erinn', 'boklm', );
+my $clone_dir = "$FindBin::Bin/clones/tbb";
+
+sub git_cmd {
+    my $oldcwd = getcwd;
+    chdir $clone_dir || exit_error "Error entering directory $clone_dir";
+    my @res = capture_exec(@_);
+    chdir $oldcwd;
+    if (!$res[2]) {
+        exit_error 'Error running ' . join(' ', @_) . ":\n" . $res[1];
+    }
+    return @res;
+}
+
+sub git_clone_pull {
+    mkdir "$FindBin::Bin/clones" unless -d "$FindBin::Bin/clones";
+    if (!-d $clone_dir) {
+        system('git', 'clone', $tbbgit_url, $clone_dir) == 0
+                || exit_error "Error cloning $tbbgit_url";
+    }
+    git_cmd('git', 'pull');
+}
+
+sub set_gpgwrapper {
+    my $keyring = '';
+    foreach my $user (@sign_users) {
+        $keyring .= " --keyring $FindBin::Bin/keyring/$user.gpg";
+    }
+    my $wrapper = <<EOF;
+#!/bin/sh
+set -e
+exec gpg --no-default-keyring $keyring --trust-model always "\$@"
+EOF
+    my $wrapper_file = "$options->{tmpdir}/gpgtbbgit";
+    write_file($wrapper_file, $wrapper);
+    chmod 0700, $wrapper_file;
+    git_cmd('git', 'config', '--replace-all', '--local',
+                'gpg.program', $wrapper_file);
+}
+
+sub latest_tagged_version {
+    git_clone_pull;
+    set_gpgwrapper;
+    my ($d) = git_cmd('git', 'describe', '--long', '--match=tbb-*', 'master');
+    my @t = split /-/, $d;
+    pop @t;
+    pop @t;
+    my $tag = join('-', @t);
+    git_cmd('git', 'tag', '-v', $tag);
+    if ($t[0] ne 'tbb' || @t != 3) {
+        exit_error "Unknown tag format $tag";
+    }
+    return ($t[1], $t[2]);
+}
+
+sub latest_builds {
+    ($options) = @_;
+    my ($version, $build) = latest_tagged_version;
+    my @res;
+    foreach my $user (@tbb_builders) {
+        my $url = "https://people.torproject.org/~$user/builds/$version/sha256sums.txt";
+        my $sha = get($url);
+        next unless $sha;
+        my $shasha = sha256_hex($sha);
+        push @res, {
+            version => $version,
+            build => $build,
+            url => $url,
+            user => $user,
+            shasha => $shasha,
+        };
+    }
+    return @res;
+}
+
+1;
