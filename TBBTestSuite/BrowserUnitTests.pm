582a8c4a17269718f57a796995a0e05e29902df9
Nicolas Vigier <boklm@torproject.org>
2014-09-11 16:59:09 +0200

    xpcshell: skip test if no xunit file was created

--- TBBTestSuite/BrowserUnitTests.pm
+++ TBBTestSuite/BrowserUnitTests.pm
@@ -225,6 +225,7 @@ sub xpcshell_test {
     my ($out, $err, $success) =
                 capture_exec('xvfb-run', '--server-args=-screen 0 1024x768x24',
                     './mach', 'xpcshell-test', $test->{dir});
+    return unless -f $xunit_file;
     $test->{results}{out} = $out;
     $test->{results}{failed} = {};
     my $root = eval {
