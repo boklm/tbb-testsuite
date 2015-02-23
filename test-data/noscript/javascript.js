68c3f6bb2ea122ac3f478f8629abba3871c0e232
Nicolas Vigier <boklm@torproject.org>
2015-02-23 18:40:54 +0100

    Start adding noscript test data

---
+++ test-data/noscript/javascript.js
@@ -0,0 +1,4 @@
+var result = document.createElement('div');
+result.innerHTML = 'JavaScriptEnabled';
+result.setAttribute("id", "test_result");
+document.getElementsByTagName("body")[0].appendChild(result);
