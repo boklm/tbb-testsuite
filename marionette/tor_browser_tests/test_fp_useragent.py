c19deee664a86c5254d83a7b2c0bfc77211c0d1c
Nicolas Vigier <boklm@torproject.org>
2017-05-10 21:23:00 +0200

    Update useragent tests for ESR52

--- marionette/tor_browser_tests/test_fp_useragent.py
+++ marionette/tor_browser_tests/test_fp_useragent.py
@@ -10,5 +10,5 @@ class Test(MarionetteTestCase):
             js = self.marionette.execute_script
             # Check that useragent string is as expected
             # We better know the ESR version we're testing
-            self.assertEqual("Mozilla/5.0 (Windows NT 6.1; rv:45.0) Gecko/20100101 Firefox/45.0",
+            self.assertEqual("Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0",
                               js("return navigator.userAgent"))
