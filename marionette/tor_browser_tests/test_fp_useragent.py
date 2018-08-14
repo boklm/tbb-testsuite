d284c7987633de7a6e96f0bd41c452ccf0d9f48c
Nicolas Vigier <boklm@torproject.org>
2018-08-14 10:26:42 +0200

    Bug 27133: update useragent in fp_useragent test

--- marionette/tor_browser_tests/test_fp_useragent.py
+++ marionette/tor_browser_tests/test_fp_useragent.py
@@ -3,6 +3,8 @@ from marionette_driver.errors import MarionetteException
 
 from marionette_harness import MarionetteTestCase
 
+import testsuite
+
 class Test(MarionetteTestCase):
     def test_useragent(self):
         with self.marionette.using_context('content'):
@@ -10,5 +12,12 @@ class Test(MarionetteTestCase):
             js = self.marionette.execute_script
             # Check that useragent string is as expected
             # We better know the ESR version we're testing
-            self.assertEqual("Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0",
+            osname = testsuite.TestSuite().t['tbbinfos']['os']
+            if osname == 'Linux':
+                ua_os = 'X11; Linux x86_64'
+            if osname == 'Windows':
+                ua_os = 'Windows NT 6.1; Win64; x64'
+            if osname == 'MacOSX':
+                ua_os = 'Macintosh; Intel Mac OS X 10.13'
+            self.assertEqual("Mozilla/5.0 (" + ua_os + "; rv:60.0) Gecko/20100101 Firefox/60.0",
                               js("return navigator.userAgent"))
